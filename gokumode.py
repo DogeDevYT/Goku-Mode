e92eef8a478729828fbbcd708135e4a6fd4d954d
DogeDevYT <52612977+DogeDevYT@users.noreply.github.com>
2023-01-17 19:06:03 -0500

    added more goku pics that go hard for the webhook
    hell yeah brother

--- gokumode.py
+++ gokumode.py
@@ -5,21 +5,21 @@ from secret import webhookurl
 webhook_url = webhookurl.url
 
 #long boi content thing a ding is role id for goku-mode
-webhook = DiscordWebhook(url=webhook_url, content="<@&1026590091889627176>")
+webhook = DiscordWebhook(url=webhook_url, content="Allright Saiyans, lets see if you're up for an AB ATTACK!!!!")
 
 #with open("goku.jpg", "rb") as f:
 #    webhook.add_file(file=f.read(), filename='goku.jpg')
-with open("goku2.png", "rb") as f:
-    webhook.add_file(file=f.read(), filename='goku2.png')
+with open("goku4.png", "rb") as f:
+    webhook.add_file(file=f.read(), filename='goku4.png')
 
 # create embed object for webhook
-embed = DiscordEmbed(title='Goku Mode', description='Let\'s get Saiyan', color='fc9803')
+embed = DiscordEmbed(title='Goku Mode', description='AB ATTACK', color='fc9803')
 
 # set author (not doing for aesthetics)
 #embed.set_author(name='Raghav', url='https://github.com/DogeDevYT', icon_url='https://avatars.githubusercontent.com/u/52612977?v=4')
 
 # set image
-embed.set_image(url='attachment://goku2.png')
+embed.set_image(url='attachment://goku4.png')
 
 # set thumbnail (not doing for aesthetics)
 #embed.set_thumbnail(url='attachment://goku.jpg')
@@ -31,8 +31,12 @@ embed.set_image(url='attachment://goku2.png')
 embed.set_timestamp()
 
 # add fields to embed
-embed.add_embed_field(name='Part 1', value='100 Push Ups')
-embed.add_embed_field(name='Part 2', value='20 Pull ups')
+embed.add_embed_field(name='Part 1', value='1 Minute Hollow Body Hold')
+embed.add_embed_field(name='Hollow Body Hold Guide', value='https://www.verywellfit.com/how-to-do-a-hollow-body-hold-techniques-benefits-variations-5079591')
+embed.add_embed_field(name='Part 2', value='30 seconds of Russian twists')
+embed.add_embed_field(name='Russian Twists Guide', value='https://www.youtube.com/watch?v=wkD8rjkodUI')
+embed.add_embed_field(name='Part 3', value='30 seconds of side planks (each side)')
+
 
 # add embed object to webhook
 webhook.add_embed(embed)
